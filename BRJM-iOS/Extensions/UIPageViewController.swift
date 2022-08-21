af78eefa6e85952b28742f9843ff0b3a0553ff3a
Hoojeong Kim <58141862+hoojeong-dev@users.noreply.github.com>
2022-08-21 16:29:17 +0900

    feat: UIPageViewController extension 구현 (#20)

---
+++ BRJM-iOS/Extensions/UIPageViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+extension UIPageViewController {
+    
+    public var isPagingEnabled: Bool {
+        get {
+            var isEnabled: Bool = true
+            for view in view.subviews {
+                if let subView = view as? UIScrollView {
+                    isEnabled = subView.isScrollEnabled
+                }
+            }
+            return isEnabled
+        }
+        set {
+            for view in view.subviews {
+                if let subView = view as? UIScrollView {
+                    subView.isScrollEnabled = newValue
+                }
+            }
+        }
+    }
+}
