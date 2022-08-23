1a3b6797eca0eee59034b979f53eb7fb195a5641
Hoojeong Kim <58141862+hoojeong-dev@users.noreply.github.com>
2022-08-23 22:06:35 +0900

    feat: 홈 테이블 뷰 구현 중 (#24)

--- BRJM-iOS/Presentation/Home/HomeViewController.swift
+++ BRJM-iOS/Presentation/Home/HomeViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import RxCocoa
+import RxSwift
 
 
 /**
@@ -7,9 +8,41 @@ import RxCocoa
  */
 class HomeViewController : BaseViewController {
 
+    /** 네비게이션 바 */
+    @IBOutlet weak var tvNavigationTitle: UILabel!
+    @IBOutlet weak var btnSearch: UIButton!
+    
+    /** 컬렉션 뷰 */
+    @IBOutlet weak var tableView: UITableView!
+    
     
     /** Lifecycle */
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /** 뷰 설정 */
+        setViews()
+    }
+    
+    /** 뷰 설정 */
+    private func setViews() {
+        
+        /** 네비게이션 타이틀 */
+        tvNavigationTitle.text = "home_text_1".localized()
+        tvNavigationTitle.textColor = BaseConstraint.COLOR_LIGHTE_GRAY
+        tvNavigationTitle.setFont(type: .regular, size: 14)
+        
+        /*
+        /** 테이블 뷰 설정 */
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CardViewCell", bundle: nil), forCellReuseIdentifier: "CardViewCell")
+        tableView.separatorStyle = .none
+        
+        /** footer 설정 */
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 34))
+        footerView.backgroundColor = .white
+        tableView.tableFooterView = footerView
+         */
     }
 }
