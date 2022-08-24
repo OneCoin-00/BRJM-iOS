4e88ff7297f62607a32caa53ca26f7c7e910c767
hoojeong-dev <58141862+hoojeong-dev@users.noreply.github.com>
2022-08-24 11:41:46 +0900

    feat: 랭킹 셀 구현 (#19)

---
+++ BRJM-iOS/Presentation/Common-Cell/RankingCell.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+
+/**
+ - note: Ranking Cell 설정
+ */
+class RankingCell: UICollectionViewCell {
+
+    @IBOutlet weak var ivPost: UIImageView!
+    @IBOutlet weak var tvTitle: UILabel!
+    @IBOutlet weak var tvWriter: UILabel!
+    @IBOutlet weak var tvViews: UILabel!
+    @IBOutlet weak var tvLikes: UILabel!
+    @IBOutlet weak var btnLike: UIButton!
+    @IBOutlet weak var ivInsignia: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        /** 이미지 */
+        ivPost.layer.cornerRadius = 4
+        
+        /** 타이틀 */
+        tvTitle.textColor = .black
+        tvTitle.setFont(type: .bold, size: 14)
+        
+        /** 작성자 */
+        tvWriter.textColor = .black
+        tvWriter.setFont(type: .regular, size: 12)
+        
+        /** 조회수 */
+        tvViews.textColor = .black
+        tvViews.setFont(type: .regular, size: 10)
+        
+        /** 좋아요 수 */
+        tvLikes.textColor = .black
+        tvLikes.setFont(type: .regular, size: 10)
+        
+        /** 휘장 이미지 */
+        ivInsignia.image = UIImage(named: "icInsignia1")
+    }
+
+    /** 셀 설정 */
+    public func setUpCell() {
+        
+    }
+}
